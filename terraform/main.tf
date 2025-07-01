291451e764a895352612b8f96bfda9d1f2443580
lokytech5 <lokytech5@gmail.com>
2025-07-01 20:49:23 +0100

    initial commit

---
+++ terraform/main.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket         = "tf-s3-demo-state"
+    key            = "dev/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "tf-s3-demo-locks"
+  }
+}
+
+resource "aws_s3_bucket" "demo" {
+  bucket        = "tf-s3-demo-${random_id.suffix.hex}"
+  force_destroy = true
+}
+
+resource "random_id" "suffix" {
+  byte_length = 4
+}
