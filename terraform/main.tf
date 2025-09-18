38195205c50e38d5955993bd15fad34d78babffe
lokytech5 <lokytech5@gmail.com>
2025-09-18 04:07:32 +0100

    Added lambda role using data import in terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -39,7 +39,7 @@ data "aws_caller_identity" "current" {}
 
 resource "aws_lambda_function" "hello_lambda" {
   function_name    = "lambda_function"
-  role             = data.aws_iam_role.lambda_role.arn
+  role             = var.lambda_role_arn
   handler          = "lambda_function.handler"
   runtime          = "python3.13"
   filename         = "${path.module}/../lambda/lambda_function.zip"
---
+++ terraform/terraform.tfvars
@@ -0,0 +1,1 @@
+lambda_role_arn = "arn:aws:iam::061039798341:role/lambda-fanout-shared-role"
